1902670c63632020e78039757e4b44309cdc579c
valecha876 <vraddhivalecha@gmail.com>
2025-07-12 15:20:06 +0530

    changes

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -10,7 +10,7 @@ const AppLayout = () => {
         <Header />
         <Outlet />
       </main>
-      <div className="p-10 text-center bg-gray-800 mt-10">Made with ❤️ by Codeverse</div>
+      <div className="p-10 text-center bg-gray-800 mt-10">Made with ❤️ by Vraddhi</div>
     </div>
   );
 };
